baff09da6cacc413a8326fdaa1c82e36fe73afc9
Camilo Hernández <me@camilohtml.com>
2020-10-02 22:20:49 +1000

    feat: Label

---
+++ src/components/ui/label/Label.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const Label = ({ children, htmlFor }) => <label htmlFor={htmlFor} className="f6 db mb2 ph1 gray ttu tracked">{children}</label >
---
+++ src/components/ui/label/Label.spec.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect'
+
+import { Label } from "./Label";
+
+describe("Label", () => {
+  it("renders a Label", () => {
+    const { container, getByText } = render(<Label htmlFor="hi">Hola</Label>);
+    expect(getByText(/hola/i)).toBeInTheDocument()
+    expect(container.firstChild).toMatchInlineSnapshot(`
+    <label
+      class="f6 db mb2 ph1 gray ttu tracked"
+      for="hi"
+    >
+      Hola
+    </label>
+  `)
+  });
+});
