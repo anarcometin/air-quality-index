35d9979de253c32625f8b01a726abe359d1e7019
Camilo Hernández <me@camilohtml.com>
2020-10-02 22:25:22 +1000

    feat: withLoading HoC

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { always, ifElse, prop } from 'ramda'
+
+const Loading = always(
+  <div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="purple">
+      <path transform="translate(0 0)" d="M0 12 V20 H4 V12z">
+        <animateTransform attributeName="transform" type="translate" values="0 0; 28 0; 0 0; 0 0" dur="1.5s" begin="0" repeatCount="indefinite" keyTimes="0;0.3;0.6;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+      </path>
+      <path opacity="0.5" transform="translate(0 0)" d="M0 12 V20 H4 V12z">
+        <animateTransform attributeName="transform" type="translate" values="0 0; 28 0; 0 0; 0 0" dur="1.5s" begin="0.1s" repeatCount="indefinite" keyTimes="0;0.3;0.6;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+      </path>
+      <path opacity="0.25" transform="translate(0 0)" d="M0 12 V20 H4 V12z">
+        <animateTransform attributeName="transform" type="translate" values="0 0; 28 0; 0 0; 0 0" dur="1.5s" begin="0.2s" repeatCount="indefinite" keyTimes="0;0.3;0.6;1" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" calcMode="spline" />
+      </path>
+    </svg>
+  </div>
+);
+export const withLoading = ifElse(prop('loading'), Loading)
